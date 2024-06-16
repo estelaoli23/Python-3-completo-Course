f8b2c4b096cfdaa8765b4412481f33dbe23cb7d6
estelaoli23 <estelaoliveirareato23@outlook.com>
2024-06-16 18:42:36 -0300

    formatando-float

---
+++ aulas/aula13.py
@@ -0,0 +1,8 @@
+nome = 'Luiz Otávio'
+altura = 1.61
+peso = 76.4
+imc = peso/(altura*altura)
+
+
+# usamos o 2f para indicar que queremos 2 casas decimais em um numero float decimal
+print(f' Estela de Oliveira tem um imc de {imc:_.2f}')
